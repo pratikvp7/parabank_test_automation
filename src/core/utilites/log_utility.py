fcefdb3d13801b567540c45d56956d5e2018c982
pratikvp7 <168306167+pratikvp7@users.noreply.github.com>
2026-01-04 23:49:07 +0530

    Added basic log utility

--- src/core/utilites/log_utility.py
+++ src/core/utilites/log_utility.py
@@ -1,9 +1,34 @@
+import datetime
+import os
+
 from root import ROOT_DIR
+from src.core.utilites.directory_handler import DirectoryHandler
 
 
 class LogUtility:
     def __init__(self):
-        pass
+        self.current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.dir_handler = DirectoryHandler()
+
+    def info(self, msg):
+        print(f"{self.current_time} [INFO] {msg}")
+
+    def debug(self, msg):
+        print(f"{self.current_time} [DEBUG] {msg}")
+
+    def warn(self, msg):
+        print(f"{self.current_time} [WARN] {msg}")
+
+    def error(self, msg):
+        print(f"{self.current_time} [ERROR] {msg}")
+
+    def __write_log(self, log_level, msg):
+        print(f"{self.current_time} [{log_level}] {msg}")
 
 
-
+if __name__ == '__main__':
+    log = LogUtility()
+    log.info("Hello")
+    log.debug("Hello")
+    log.warn("Hello")
+    log.error("Hello")
